bd3766801dd5d0863ead6f7cf061065a72419e9a
Betul <betulkoru06@gmail.com>
2024-02-02 16:21:09 +0300

    Header.jsx button added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,6 +5,13 @@ const Header = ({categories, text}) => {
   return (
     <div>
       <h1>{text}</h1>
+      <div>
+        {
+          categories.map((item) => (
+            <button key={item}>{item}</button>
+          ))
+        }
+      </div>
     </div>
   )
 }

c222800537887f5181e3a94e425200254bf0ce49
Betul <betulkoru06@gmail.com>
2024-02-02 17:04:54 +0300

    products

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,8 +1,23 @@
 
 
-const ProductList = () => {
+const ProductList = ({products}) => {
+  console.log(products);
   return (
-    <h1>ProductList</h1>
+    <div className="card-container">
+      {
+        products.map((product) => (
+          <div key={product.id}>
+            <div>
+              <h3>{product.price}</h3>
+            </div>
+            <img src={product.image} alt={product.title} width="100px" />
+            <div>
+              <h2>{product.title}</h2>
+            </div>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 

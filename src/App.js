affc9b041f4e1cfcc83a1c6c172d71a5209c4815
Betul <betulkoru06@gmail.com>
2024-02-02 20:25:17 +0300

    Hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Header from "./components/header/Header";
+// import Header from "./components/header/Header";
 import ProductList from "./components/products/ProductList";
 import { products, categories } from "./helper/data";
 import "./App.scss"
+import HeaderH from "./components/headerHam/HeaderH";
 
 function App() {
   // console.log(products);
   return (
     <div>
-      <Header categories={categories} text="Products List"/>
-      <ProductList products={products}/>
+      {/* <Header categories={categories} text="Products List"/> */}
+      <HeaderH categories={categories} />
+      <ProductList products={products} />
     </div>
   );
 }
